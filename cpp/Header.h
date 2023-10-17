4959be43df91bed410eb2117794e3b72f5451034
Yash Pincha <142161252+yashpincha@users.noreply.github.com>
2023-10-17 12:18:14 +0530

    Update Header.h

--- cpp/Header.h
+++ cpp/Header.h
@@ -1,80 +1,90 @@
-#ifndef Header_h 
+#ifndef Header_h
 #define Header_h
 
-class pendulum{
+class Pendulum {
 private:
+    // Time and identification
     double t;
-    int num ;
-    // position
-    double x,dx,ddx;
-    double y,dy,ddy;
-    // angles
-    double theta,omega,domega ;
-    // system params (mass and length)
-    double length;
-    double mass ;
+    int num;
 
-    double energy ;
+    // Position variables
+    double x, dx, ddx;
+    double y, dy, ddy;
+
+    // Angular variables
+    double theta, omega, domega;
+
+    // System parameters (mass and length)
+    double length;
+    double mass;
+
+    // Energy variables
+    double energy;
     double Ek;
     double Ep;
-    public :
-    
-    pendulum(){};
-    pendulum(int,double,double,double,double,double,double);
-    ~pendulum(){};
-      
-    // init
-    void init(int,double,double,double,double,double,double);
-    void coordinates();
-    
-    //Energy
-    void Energy(); 
-    void ek();
-    void ep();
-    
-    // Gets
-    double GetEnergy() const {return energy;}
-    double GetEk() const {return Ek;}
-    double GetEp() const {return Ep;}
-    int GetNum() const {return num;}
-    double GetT() const{return t;}
-    double GetX() const{return x;}
-    double GetY()const {return y;}
-    double GetDx()const {return dx;}
-    double GetDdx()const{return ddx;}
-    double GetDy()const{return dy;}
-    double GetDdy() const{return ddy ;}
-    double GetTheta()const{return theta ;}
-    double GetOmega()const{return omega ;}
-    double GetDomega()const{return domega ;}
-    double GetLength() const {return length;}
-    double GetMass() const {return mass ;}
 
-    // Sets
-    void SetEnergy(double a){energy=a;}
-    void SetEk(double a){Ek=a;}
-    void SetEp(double a){Ep=a;}
-    void SetNum(double a){num=a;}
-    void SetT(double a){t=a;}
-    void SetX(double a){x=a;}
-    void SetY(double a){y=a;}
-    void SetDx(double a){dx=a;}
-    void SetDy(double a){dy=a;}
-    void SetDdx(double a){ddx=a;}
-    void SetDdy(double a){ddy=a;}
-    void SetTheta(double a){theta=a;}
-    void SetOmega(double a){omega=a;}
-    void SetDomega(double a){domega=a;}
-    void SetLength(double a){length=a;}
-    void SetMass(double a){mass=a;}
-    
-    // Increment += 
-    void AddX(double a){x+=a;}
-    void AddY(double a){y+=a;}
-    void AddDx(double a){dx+=a;}
-    void AddDy(double a){dy+=a;}
-    void AddDdx(double a){ddx+=a;}
-    void AddDdy(double a){ddy+=a;}
+public:
+    // Constructors and Destructor
+    Pendulum();
+    Pendulum(int num, double t, double x, double dx, double ddx, double y, double dy, double ddy,
+              double theta, double omega, double domega, double length, double mass);
+    ~Pendulum();
+
+    // Initialization
+    void init(int num, double t, double x, double dx, double ddx, double y, double dy, double ddy,
+              double theta, double omega, double domega, double length, double mass);
+
+    // Coordinate calculations
+    void calculateCoordinates();
+
+    // Energy calculations
+    void calculateEnergy();
+    void calculateEk();
+    void calculateEp();
+
+    // Getters
+    double getEnergy() const;
+    double getEk() const;
+    double getEp() const;
+    int getNum() const;
+    double getT() const;
+    double getX() const;
+    double getY() const;
+    double getDx() const;
+    double getDdx() const;
+    double getDy() const;
+    double getDdy() const;
+    double getTheta() const;
+    double getOmega() const;
+    double getDomega() const;
+    double getLength() const;
+    double getMass() const;
+
+    // Setters
+    void setEnergy(double value);
+    void setEk(double value);
+    void setEp(double value);
+    void setNum(int value);
+    void setT(double value);
+    void setX(double value);
+    void setY(double value);
+    void setDx(double value);
+    void setDy(double value);
+    void setDdx(double value);
+    void setDdy(double value);
+    void setTheta(double value);
+    void setOmega(double value);
+    void setDomega(double value);
+    void setLength(double value);
+    void setMass(double value);
+
+    // Increment operators
+    void addToX(double value);
+    void addToY(double value);
+    void addToDx(double value);
+    void addToDy(double value);
+    void addToDdx(double value);
+    void addToDdy(double value);
 };
 
-#endif
+#endif  // Header_h
